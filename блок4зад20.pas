b53051b59dc8d1a903a1a94cb6ab283bb36c3ddc
chaf666 <144238017+chaf666@users.noreply.github.com>
2023-09-26 08:38:53 +0300

    Add files via upload

---
+++ блок4зад20.pas
@@ -0,0 +1,11 @@
+﻿program blok4zad20;
+var x,y,x1,y1:integer;
+begin
+  writeln('Введите координаты точек: ');
+  read(x,y,x1,y1);
+  if (x>0) and (y>0) and (x1>0) and (y1>0) then writeln('Лежат в одной плоскости')
+  else if (x<0) and (y>0) and (x1<0) and (y1>0) then writeln('Лежат в одной плоскости')
+       else if (x<0) and (y<0) and (x1<0) and (y1<0) then writeln('Лежат в одной плоскости')
+            else if (x>0) and (y<0) and (x1>0) and (y1<0) then writeln('Лежат в одной плоскости')
+                 else writeln('Не лежат в одной плоскости');
+end.
---
+++ блок4зад22.pas
@@ -0,0 +1,8 @@
+﻿program blok4zad22;
+var x1,y1,x2,y2:integer;
+begin
+writeln('Введите координаты: ');
+read(x1,y1,x2,y2);
+if (x1=x2) or (y1=y2) then writeln('Ладья бьет эту фигуру')
+else writeln('Ладья не бьет эту фигуру');
+end.
